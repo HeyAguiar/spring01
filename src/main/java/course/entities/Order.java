3788a23e663046f3ff03e15644af6079b7951743
Guilherme Aguiar <mechamedetaki@gmail.com>
2023-07-13 19:05:28 -0300

    Payment, one-to-one association

--- src/main/java/course/entities/Order.java
+++ src/main/java/course/entities/Order.java
@@ -2,15 +2,20 @@ package course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import course.entities.enums.OrderStatus;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -28,6 +33,12 @@ public class Order implements Serializable {
    @ManyToOne
    @JoinColumn(name = "client_id")
    private User client;
+   
+   @OneToMany(mappedBy = "id.order")
+   private Set<OrderItem> items = new HashSet<>();
+   
+   @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+   private Payment payment;
    
    public Order() {
 	   
@@ -67,6 +78,15 @@ public class Order implements Serializable {
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	public Integer getOrderStatus() {
